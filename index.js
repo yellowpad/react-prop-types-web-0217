c5ba3af012792599c5c6acfdfb721ae88a8fce82
dgwp-projects <zayneabraham1@gmail.com>
2017-04-04 13:49:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const app = "I don't do much.";
+class Order extends React.Component {
+  render() {
+    return (
+      <div className="order">
+        <ul>
+          <li>{this.props.cone ? 'Cone' : 'Cup'}</li>
+          <li>{this.props.size}</li>
+          <li>{this.props.scoops.length} scoops: {this.props.scoops.join(', ')}</li>
+          <li>Ordered by {this.props.orderInfo.customerName} at {this.props.orderInfo.orderedAt}.</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+
+class Order extends React.Component {
+  render() {
+    // ...
+  }
+}
+
+Order.defaultProps = {
+  cone: true,
+  size: 'regular'
+};
+
+
+Order.propTypes = {
+  cone: React.PropTypes.bool,
+  size: React.PropTypes.string,
+  scoops: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
+  orderInfo: React.PropTypes.shape({
+  	customerName: React.PropTypes.string.isRequired,
+  	orderedAt: React.PropTypes.number.isRequired
+  }).isRequired
+};
